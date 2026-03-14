acfd0c13cb98948dde446e4ed014732fbfde630d
Juan Toro <juancarlostorocifuentes12@gmail.com>
2026-03-13 21:42:27 -0500

    feat(subactivities): add POST endpoint to validate tentative date workload and suggestions

--- subactivities/api/serializer.py
+++ subactivities/api/serializer.py
@@ -6,3 +6,8 @@ class SubActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = SubActivity
         fields = '__all__'
+
+
+class ValidateSubactivityTentativeDateSerializer(serializers.Serializer):
+    subactivity = serializers.IntegerField(required=True)
+    tentative_date = serializers.DateTimeField(required=True)
